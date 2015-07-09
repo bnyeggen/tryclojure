2e151cd6f6a1f2564dbfee5d51b2cab70917c734
Bryce Nyeggen <fiat.money@gmail.com>
2015-07-08 19:51:43 -0700

    Remove all actual tutorial functionality & update dependencies

--- src/tryclojure/server.clj
+++ src/tryclojure/server.clj
@@ -4,14 +4,10 @@
             [noir.util.middleware :as nm]
             [ring.adapter.jetty :as jetty]
             [tryclojure.views.home :as home]
-            [tryclojure.views.tutorial :as tutorial]
             [tryclojure.views.eval :as eval]))
 
 (def app-routes
   [(GET "/" [] (home/root-html))
-   (GET "/about" [] (home/about-html))
-   (GET "/links" [] (home/links-html))
-   (POST "/tutorial" [:as {args :params}] (tutorial/tutorial-html (args :page)))
    (POST "/eval.json" [:as {args :params}] (eval/eval-json (args :expr) (args :jsonp)))
    (GET "/eval.json" [:as {args :params}] (eval/eval-json (args :expr) (args :jsonp)))
    (route/resources "/")
--- src/tryclojure/views/tutorial.clj
+++
@@ -1,4 +0,0 @@
-(ns tryclojure.views.tutorial)
-
-(defn tutorial-html [page] 
-  (slurp (str "resources/public/tutorial/" page ".html")))
